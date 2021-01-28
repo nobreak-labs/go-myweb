bc31ba1c6218a7c9f57c98e08ec4116275f103c2
Ryan Jang <c1t1d0s7@gmail.com>
2021-01-28 11:03:39 +0900

    Add query string

--- README.md
+++ README.md
@@ -1,32 +1,55 @@
 go-myweb Web Application
 ========================
 
-- Default Port: 8080
+### Default Port
+```8080```
 
-- Change Service Port
-```shell
-./myweb -port=8088
+### Default Response
+```
+Hello World!
+localhost
 ```
 
-- Change Message
-```
-MESSAGE="Hello Myweb" ./myweb
-```shell
+### Detailed Response
+```http://X.X.X.X/?detail=V```
 
-- Http Response
-```html
+- detail
+   	- Numeric
+       	- 1
+       	- 2
+       	- 3
+   	- Strings
+       	- header
+       	- client
+       	- container
+
+```
+Hello World!
+localhost
 [Request Headers]
-	GET / HTTP/1.1
+	GET /?detail=3 HTTP/1.1
 	User-Agent: [curl/7.64.1]
 	Accept: [*/*]
 [Client Informations]
 	Host: localhost:8080
-	RemoteAddr: [::1]:61842
-[Host Informations]
+	RemoteAddr: [::1]:62949
+[Container Informations]
 	Hostname: Ryan-MBP.local
 	IP: 192.168.0.12
 	DNS: [1.1.1.1 1.0.0.1], []
-	Uptime: 15D, 4H, 19M
-	Env Message: Hello world
+	Uptime: 3D, 2H, 58M%
 ```
 
+### Change Service Port
+```shell
+./myweb -port=8088
+```
+
+### Change Message
+```shell
+MESSAGE="Hello Myweb" ./myweb
+```
+```
+Hello Myweb!
+localhost
+```
--- myweb.go
+++ myweb.go
@@ -13,8 +13,6 @@ import (
 	"github.com/shirou/gopsutil/host"
 )
 
-const defaultEnvMessage = "Hello world"
-
 //GetOutboundIP function gets Outbound IP
 func GetOutboundIP() net.IP {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
@@ -63,7 +61,7 @@ func checkErr(err error) {
 	}
 }
 
-//RootHandler function
+//rootHandler function
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		log.Fatalln(err)
@@ -72,40 +70,99 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 	//Logging: request of client address
 	log.Printf("Request %v --%v--> %v%v\n", r.RemoteAddr, r.Method, r.Host, r.URL)
 
-	//Printing: http request header (method, url, protocol)
-	fmt.Fprintf(w, "[Request Headers]")
-	fmt.Fprintf(w, "\n\t%v %v %v\n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "\t%v: %v\n", k, v)
+	query := r.URL.Query()
+	detail := query.Get("detail")
+
+	if detail == "" {
+		detail = "0"
 	}
 
-	//Printing: request host & remote address
-	fmt.Fprintf(w, "[Client Informations]")
-	fmt.Fprintf(w, "\n\tHost: %v", r.Host)
-	fmt.Fprintf(w, "\n\tRemoteAddr: %v\n", r.RemoteAddr)
+	detailNum, err := strconv.Atoi(detail)
+	if err != nil {
+		detailNum = 0
+	}
 
-	//Printing: Host Information
-	fmt.Fprintf(w, "[Host Informations]")
-	fmt.Fprintf(w, "\n\tHostname: %v", GetHostname())
-	fmt.Fprintf(w, "\n\tIP: %v", GetOutboundIP())
-	fmt.Fprintf(w, "\n\tDNS: %v", GetDNSServer())
-	fmt.Fprintf(w, "\n\tUptime: %v", GetUptime())
-	fmt.Fprintf(w, "\n\tEnv Message: %v\n", setEnvMessage())
+	//Printing: welcome message with hostname
+	fmt.Fprintf(w, welcomeMessage())
+	fmt.Fprintf(w, GetHostname())
+
+	if detailNum >= 1 || detail == "header" {
+		//Printing: http request header (method, url, protocol)
+		fmt.Fprintln(w)
+		fmt.Fprintf(w, "[Request Headers]")
+		fmt.Fprintf(w, "\n\t%v %v %v\n", r.Method, r.URL, r.Proto)
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "\t%v: %v\n", k, v)
+		}
+	}
+	if detailNum >= 2 || detail == "client" {
+		//Printing: request host & remote address
+		fmt.Fprintf(w, "[Client Informations]")
+		fmt.Fprintf(w, "\n\tHost: %v", r.Host)
+		fmt.Fprintf(w, "\n\tRemoteAddr: %v\n", r.RemoteAddr)
+	}
+	if detailNum >= 3 || detail == "container" {
+		//Printing: container information
+		fmt.Fprintf(w, "[Container Informations]")
+		fmt.Fprintf(w, "\n\tHostname: %v", GetHostname())
+		fmt.Fprintf(w, "\n\tIP: %v", GetOutboundIP())
+		fmt.Fprintf(w, "\n\tDNS: %v", GetDNSServer())
+		fmt.Fprintf(w, "\n\tUptime: %v", GetUptime())
+	}
 }
 
-//NotFoundHandler for /404
-func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprintf(w, "Oops! 404 Not Found!")
+//healthCheckHandler function
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	code := query.Get("code")
+
+	if code == "" {
+		code = "200"
+	}
+
+	codeNum, err := strconv.Atoi(code)
+	if err != nil {
+		codeNum = 999
+	}
+
+	if codeNum == 0 || codeNum == 200 {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Health Check: OK")
+		log.Println("Health Check: OK")
+	} else if codeNum >= 100 && codeNum < 199 {
+		w.WriteHeader(codeNum)
+		fmt.Fprintln(w, "Health Check: Informational")
+		log.Println("Health Check: Informational")
+	} else if codeNum >= 201 && codeNum < 299 {
+		w.WriteHeader(codeNum)
+		fmt.Fprintln(w, "Health Check: Success")
+		log.Println("Health Check: Success")
+	} else if codeNum >= 300 && codeNum < 399 {
+		w.WriteHeader(codeNum)
+		fmt.Fprintln(w, "Health Check: Redirection")
+		log.Println("Health Check: Redirection")
+	} else if codeNum >= 400 && codeNum < 499 {
+		w.WriteHeader(codeNum)
+		fmt.Fprintln(w, "Health Check: Client Error")
+		log.Println("Health Check: Client Error")
+	} else if codeNum >= 500 && codeNum < 599 {
+		w.WriteHeader(codeNum)
+		fmt.Fprintln(w, "Health Check: Server Error")
+		log.Println("Health Check: Server Error")
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Health Check: No Valid Code")
+		log.Println("Health Cehck: No Valid Code")
+	}
 }
 
-//setEnvMessage
-func setEnvMessage() string {
+//welcomeMessage
+func welcomeMessage() string {
 	m := os.Getenv("MESSAGE")
-	if m != "" {
-		return m
+	if m == "" {
+		m = "Hello World!"
 	}
-	return defaultEnvMessage
+	return m + "\n"
 }
 
 func main() {
@@ -116,7 +173,7 @@ func main() {
 	log.Println("Listen: ", "0.0.0.0:"+strconv.Itoa(*p))
 
 	http.HandleFunc("/", RootHandler)
-	http.HandleFunc("/404", NotFoundHandler)
+	http.HandleFunc("/health", HealthCheckHandler)
 	log.Fatal(
 		http.ListenAndServe("0.0.0.0:"+strconv.Itoa(*p), nil),
 	)
